a236dedd263c115594fc9eff971f4d64d12b28fc
aeingza1 <38566488+aeingza1@users.noreply.github.com>
2020-04-27 23:17:46 +0700

    add ExchangeRate

--- src/main/java/com/angular/donationblock/entity/BaseEntity.java
+++ src/main/java/com/angular/donationblock/entity/BaseEntity.java
@@ -8,25 +8,30 @@ import javax.persistence.*;
 @Getter
 @Setter
 @MappedSuperclass
-public class BaseEntity {
+public class BaseEntity 
+{
     @Id
     @GeneratedValue
     private Long id;
     private boolean deleted;
 
-    public Long getId() {
+    public Long getId() 
+    {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id) 
+    {
         this.id = id;
     }
 
-    public boolean isDeleted() {
+    public boolean isDeleted() 
+    {
         return deleted;
     }
 
-    public void setDeleted(boolean deleted) {
+    public void setDeleted(boolean deleted) 
+    {
         this.deleted = deleted;
     }
 }
